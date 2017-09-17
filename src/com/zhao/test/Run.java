46b0beebb6b5d669c4d8163b10d5672e544f0b5e
zhao <1610803142@qq.com>
2017-09-17 21:17:26 +0800

    改变时间onchange学习

---
+++ src/com/zhao/test/Run.java
@@ -0,0 +1,16 @@
+package com.zhao.test;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		for(int i=1;i<=100;i++){
+			
+			sum+=i;
+		
+
+	}
+     System.out.println(sum);
+}
+}
